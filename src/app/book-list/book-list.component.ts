7d2c4df68b43c3b285a45bd3103b8c4f1ca3192d
pedrohgarbim <phg.garbim@gmail.com>
2024-09-01 16:12:45 -0300

    ngFor loop

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Book } from '../book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
+
+  books: Book[] = [
+    { id: 1, author: "Leo Tolstoy", title: "War and Peace" },
+    { id: 2, author: "Leo Tolstoy", title: "Anna Karenina" },
+    { id: 3, author: "Mikhail Bulgakov", title: "The Master and Margarita" },
+    { id: 4, author: "Julius Caesar", title: "The Gallic Wars" }
+
+  ]
 
 }
